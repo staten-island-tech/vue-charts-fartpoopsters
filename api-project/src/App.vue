6d040f2472b88d9935cef7dc16a3328e173e7294
Constantine-nagornuk <124071530+Constantine-nagornuk@users.noreply.github.com>
2024-03-07 13:08:08 -0500

    idk i broke it

--- api-project/src/App.vue
+++ api-project/src/App.vue
@@ -1,11 +1,11 @@
 <template>
-  
+    <Chartthangtest/>
 
 </template>
 
 
 <script setup>
-
+import Chartthangtest from './components/Chartthangtest.vue'
 
 </script>
 
---
+++ api-project/src/components/Chartthangtest.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container">
+    <Bar v-if="loaded" :data="chartData" />
+  </div>
+</template>
+
+  <script setup>
+
+import { Bar } from 'vue-chartjs'
+import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
+import {ref, onBeforeMount} from "vue"
+const thang = ref("")
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+
+async function DOHMH (){
+let res = await fetch("https://data.cityofnewyork.us/resource/knr6-vurn.json")
+let data = await res.json()
+thang.value = data.results
+
+}
+onBeforeMount(() =>{DOHMH})
+</script>
+
+
+<style scoped>
+
+
+</style>
