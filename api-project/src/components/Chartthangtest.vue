79fe9e032f47736a5c359129a395805ab6a205d6
Constantine-nagornuk <124071530+Constantine-nagornuk@users.noreply.github.com>
2024-03-08 13:13:47 -0500

    if it works ill eat poop

--- api-project/src/components/Chartthangtest.vue
+++ api-project/src/components/Chartthangtest.vue
@@ -5,19 +5,24 @@
 </template>
 
   <script setup>
-
 import { Bar } from 'vue-chartjs'
 import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
 import {ref, onBeforeMount} from "vue"
-const thang = ref("")
+
 ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
 
+const thang = ref("")
+
 async function DOHMH (){
-let res = await fetch("https://data.cityofnewyork.us/resource/knr6-vurn.json")
+  this.loaded = false
+try{
+  let res = await fetch("https://data.cityofnewyork.us/resource/knr6-vurn.json")
 let data = await res.json()
 thang.value = data.results
-
+this.loaded = true
 }
+catch (error){
+       error } }
 onBeforeMount(() =>{DOHMH})
 </script>
 
@@ -26,3 +31,6 @@ onBeforeMount(() =>{DOHMH})
 
 
 </style>
+
+
+
