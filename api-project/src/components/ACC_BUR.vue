df1a41ccd40a1c363fbee7cfec480436f7c5c8ca
Constantine-nagornuk <124071530+Constantine-nagornuk@users.noreply.github.com>
2024-03-26 13:12:31 -0400

    pie chart is not as ugly

--- api-project/src/components/ACC_BUR.vue
+++ api-project/src/components/ACC_BUR.vue
@@ -50,12 +50,12 @@ export default {
           label: 'Collisions involving vehicles managed by Department of Health and Mental Hygiene (DOHMH) by Borough',
           data: [SI1, BX1, Brooklyn1, Manhattan1, Queens1, Yonkers1],
           backgroundColor: [
-            'rgba(255, 99, 132, 0.2)',
-            'rgba(54, 162, 235, 0.2)',
-            'rgba(255, 206, 86, 0.2)',
-            'rgba(75, 192, 192, 0.2)',
-            'rgba(153, 102, 255, 0.2)',
-            'rgba(255, 159, 64, 0.2)'
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)'
           ],
           borderColor: [
             'rgba(255, 99, 132, 1)',
